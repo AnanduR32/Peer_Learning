cacd8d55137ed608d2a80e1cfd833f54082ee28a
Anandu R <32507674+AnanduR32@users.noreply.github.com>
2020-03-17 12:04:10 +0530

    Update peer.dart

--- peer.dart
+++ peer.dart
@@ -30,7 +30,7 @@ class Peer{
 }
 main(){
     user['admin']={'mentor',{'dart'},{9,17}};
-    print('\nWelcome to peer learning\nChoose an option:\n\t1.Enter new user\n\t2.Update existing stack\n\t3.Set user designation\n\tSet Working hours\n\tSearch for users in working time\n');
+    print('\nWelcome to peer learning\nChoose an option:\n\t1.Enter new user\n\t2.Update existing stack\n\t3.Set user designation\n\t4.Set Working hours\n\t5.Search for users in working time\n');
     int ch = int.parse(stdin.readLineSync());
     switch(ch){
       case 1:
