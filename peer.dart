45e04753fcdee2fd8083da79833c1b729f4ad619
Anandu R <32507674+AnanduR32@users.noreply.github.com>
2020-03-17 11:44:49 +0530

    Update peer.dart

--- peer.dart
+++ peer.dart
@@ -7,13 +7,17 @@ class Peer{
   }setMentorOrLearner(int id){  //Set whether the participant is learner or mentor
     print('Is user mentor or learner: ');
     var desig = stdin.readLineSync();
-    if(desig=='learner'){
-      user[id][0]='Learner';
-    else
-      user[id][0]='Mentor';
+    if(desig.toLowerCase()=='learner'){
+      user[id][0]='learner';
+    }else if(desig.toLowerCase()=='mentor')
+      user[id][0]='mentor';
     }
-  }setAvailableTime(){  //if person is mentor set available time
-
+  }setAvailableTime(int id){  //if person is mentor set available time
+    if(user[id][0]=='mentor'){
+      print('Enter working hours: ');
+      var time = stdin.readLineSync();
+      time.split(' ');                     
+    }
   }getMentor(){  //Takes stack and time as params and finds available mentors. 
   }main(){
     user['admin']={'mentor',{'dart'},{9,17}};
