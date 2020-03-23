36d3fc1731657acd9dff870177fdc8b6f8a54e9c
anandu R <ananduraj32@gmail.com>
2020-03-23 18:33:15 +0530

    checkpoint added color theme color.dart
    set elevation: 0.0, for appbar widget

--- Assignments/q_task_2/lib/colors.dart
+++ Assignments/q_task_2/lib/colors.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
-const bgColor = const Color(0xFFECECD7);
-const coralColor = const Color(0xFFFF7E6B);
-const raspColor = const Color(0xFF96616B);
-const mintColor = const Color(0xFFA9F0D1);
-const salmonColor = const Color(0xFFFFA69E);
+import 'package:hexcolor/hexcolor.dart';
+
+// var bgColor = Hexcolor('#ECECD7');
+// var coralColor = Hexcolor('#FF7E6B');
+// var raspColor = Hexcolor('#96616B');
+// var mintColor = Hexcolor('#A9F0D1');
+// var salmonColor = Hexcolor('#FFA69E');
+Color bgColor = const Color(0xFFECECD7);
+Color coralColor = const Color(0xFFFF7E6B);
+Color raspColor = const Color(0xFF96616B);
+Color mintColor = const Color(0xFFA9F0D1);
+Color salmonColorh=const Color(0xFFFFA69E);
--- Assignments/q_task_2/lib/main.dart
+++ Assignments/q_task_2/lib/main.dart
@@ -5,22 +5,16 @@ import 'user.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: bgColor,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
+  MyHomePage({Key key}) : super(key: key);
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
@@ -29,8 +23,10 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: bgColor,
       appBar: AppBar(
-          title: Text(widget.title),
+          elevation: 0.0,
+          backgroundColor: bgColor,
           leading: new IconButton(
             icon: new Icon(Icons.arrow_back, color: raspColor),
             onPressed: () => Navigator.of(context).pop(),
@@ -39,9 +35,9 @@ class _MyHomePageState extends State<MyHomePage> {
             color: raspColor,
           ),
           actions: <Widget>[
-            FlatButton.icon(
-              icon: Icon(Icons.person),
-              label: Text('Logout'),
+            FloatingActionButton(
+              child: Icon(Icons.person),
+              backgroundColor: raspColor,
               onPressed: () {
                 Navigator.push(
                   context,
