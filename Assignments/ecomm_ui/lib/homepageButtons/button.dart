e998b3c47df16d464e44385d7d1690a60c40a11b
Anandu R <ananduraj32@gmail.com>
2020-03-29 17:30:04 +0530

    fixed overflow by removing rawmaterialbutton

--- Assignments/ecomm_ui/lib/homepageButtons/button.dart
+++ Assignments/ecomm_ui/lib/homepageButtons/button.dart
@@ -10,72 +10,61 @@ class Button extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      crossAxisAlignment: CrossAxisAlignment.center,
       children: <Widget>[
         Container(
           width: MediaQuery.of(context).size.width * 0.40,
-          child: RawMaterialButton(
-            padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
-            shape: new RoundedRectangleBorder(
-                borderRadius: new BorderRadius.circular(18.0),
-                side: BorderSide(color: Colors.transparent)),
-            fillColor: salmonColor,
-            splashColor: beige,
-            textStyle: TextStyle(
-                color: Colors.black,
-                fontWeight: FontWeight.bold,
-                fontSize: 20.0),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                Container(
-                  child: Image.asset(
-                    item.image,
+          padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+          decoration: new BoxDecoration(
+            borderRadius: new BorderRadius.circular(18.0),
+            color: salmonColor,
+            // side: BorderSide(color: Colors.transparent),
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Container(
+                child: Image.asset(
+                  item.image,
+                ),
+                height: 100,
+                width: 100,
+              ),
+              new Padding(
+                padding: const EdgeInsets.only(top: 2.0),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        item.name,
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                      Text(
+                        item.sub,
+                        style: TextStyle(
+                          color: Colors.grey[600],
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                      Text(
+                        item.price,
+                        style: TextStyle(
+                          color: Colors.grey[700],
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ],
                   ),
-                  height: 100,
-                  width: 100,
-                ),
-                new Padding(
-                  padding: const EdgeInsets.only(top: 2.0),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: <Widget>[
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          item.name,
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontWeight: FontWeight.w700,
-                          ),
-                        ),
-                        Text(
-                          item.sub,
-                          style: TextStyle(
-                            color: Colors.grey[600],
-                            fontWeight: FontWeight.w400,
-                          ),
-                        ),
-                        Text(
-                          item.price,
-                          style: TextStyle(
-                            color: Colors.grey[700],
-                            fontWeight: FontWeight.w500,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            onPressed: () {
-              // Navigator.push(
-              //           context,
-              //           MaterialPageRoute(builder: (context) => MedPage(item)),
-              //         );
-            },
+                ],
+              ),
+            ],
           ),
         ),
         // Padding(
